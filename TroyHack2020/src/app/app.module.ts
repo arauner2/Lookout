e6e0f000b31956d714f7ab091bc3f6d50e03fe7d
Allie Rauner <akrauner@gmail.com>
2020-01-18 12:43:30 -0600

    initial add firebase

--- TroyHack2020/src/app/app.module.ts
+++ TroyHack2020/src/app/app.module.ts
@@ -8,6 +8,19 @@ import { HomeComponent } from './components/home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStoreModule } from '@angular/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA54EFJXhzRQgc5e0P-d6Ug44E3sG5gnPs",
+  authDomain: "troyhack2020.firebaseapp.com",
+  databaseURL: "https://troyhack2020.firebaseio.com",
+  projectId: "troyhack2020",
+  storageBucket: "troyhack2020.appspot.com",
+  messagingSenderId: "1013200224123",
+  appId: "1:1013200224123:web:1793bbd0627aba20de07de"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +28,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireStoreModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule
